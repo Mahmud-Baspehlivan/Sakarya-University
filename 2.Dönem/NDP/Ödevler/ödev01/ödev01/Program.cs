84b4d2b94e952de3a24ac75e03b57d25ef0ac26a
Mahmud-Baspehlivan <93977436+Mahmud-Baspehlivan@users.noreply.github.com>
2022-06-08 09:20:20 +0300

    Projeler

---
+++ 2.Dönem/NDP/Ödevler/ödev01/ödev01/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+public class PasswordStrengthChecker
+{
+    static void Main()
+    { }
+    private int GetLengthScore(string password)
+    {
+        return Math.Min(10, password.Length) * 6;
+    }
+
+    private int GetLowerScore(string password)
+    {
+        int rawScore = password.Length - Regex.Replace(password, "[a-z]", "").Length;
+        return Math.Min(2, rawScore) * 5;
+    }
+
+    private int GetUpperScore(string password)
+    {
+        int rawScore = password.Length - Regex.Replace(password, "[A-Z]", "").Length;
+        return Math.Min(2, rawScore) * 5;
+    }
+
+    private int GetDigitScore(string password)
+    {
+        int rawScore = password.Length - Regex.Replace(password, "[0-9]", "").Length;
+        return Math.Min(2, rawScore) * 5;
+    }
+
+    private int GetSymbolScore(string password)
+    {
+        int rawScore = Regex.Replace(password, "[a-zA-Z0-9]", "").Length;
+        return Math.Min(2, rawScore) * 5;
+    }
+    public int GeneratePasswordScore(string password)
+    {
+        if (password == null)
+        {
+            return 0;
+        }
+
+        int lengthScore = GetLengthScore(password);
+        int lowerScore = GetLowerScore(password);
+        int upperScore = GetUpperScore(password);
+        int digitScore = GetDigitScore(password);
+        int symbolScore = GetSymbolScore(password);
+
+        return lengthScore + lowerScore + upperScore + digitScore + symbolScore;
+    }
+    public enum PasswordStrength
+    {
+        Unacceptable,
+        Weak,
+        Normal,
+        Strong,
+        Secure
+    }
+    public PasswordStrength GetPasswordStrength(string password)
+    {
+        int score = GeneratePasswordScore(password);
+
+        if (score < 50)
+            return PasswordStrength.Unacceptable;
+        else if (score < 60)
+            return PasswordStrength.Weak;
+        else if (score < 80)
+            return PasswordStrength.Normal;
+        else if (score < 90)
+            return PasswordStrength.Strong;
+        else
+            return PasswordStrength.Secure;
+    }
+//    PasswordStrengthChecker checker = new PasswordStrengthChecker();
+//    int score;
+
+//    score = checker.GeneratePasswordScore("pwd");           /// 28
+//    score = checker.GeneratePasswordScore("password");      /// 58
+//    score = checker.GeneratePasswordScore("P45Sword");      /// 78
+//    score = checker.GeneratePasswordScore("P45Sword!");     /// 89
+//    score = checker.GeneratePasswordScore("ASriws34#!");    /// 100
+ 
+//    PasswordStrength strength;
+
+//        strength = checker.GetPasswordStrength("pwd");          /// Kabul Edilemez
+//    strength = checker.GetPasswordStrength("password");     /// Zayif
+//    strength = checker.GetPasswordStrength("P45Sword");     /// Normal
+//    strength = checker.GetPasswordStrength("P45Sword!");    /// Guclu
+//    strength = checker.GetPasswordStrength("ASriws34#!");   /// Guvenli
+}
---
+++ 2.Dönem/NDP/Ödevler/ödev02/ödev02/Program.cs
@@ -0,0 +1,182 @@
+﻿/****************************************************************************
+**					      SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				     BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				    NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					     2021-2022 BAHAR DÖNEMİ
+**	
+**				ÖDEV NUMARASI...: NDP 1.Ödev
+**				ÖĞRENCİ ADI.....: Mahmud Emre Başpehlivan
+**				ÖĞRENCİ NUMARASI: G211210020
+**              DERSİN ALINDIĞI GRUP...: 2.Öğretim A Grubu
+****************************************************************************/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ödev02
+{
+    public static class ParolaGuvenligi
+    {
+        public static int KüçükHarf(string Password) // Şifrede ki küçük harf sayısını hesaplayan metod.
+        {
+            int sayac=0;
+            for (int i = 0; i < Password.Length; i++)
+                if (char.IsLower(Password[i]))
+                    sayac += 1;
+            return sayac*10;
+        }
+        public static int BüyükHarf(string Password) // Şifrede ki büyük harf sayısını hesaplayan metod.
+        {
+            int sayac = 0;
+            for (int i = 0; i < Password.Length; i++)
+                if (char.IsUpper(Password[i]))
+                    sayac += 1;
+            return sayac*10;
+        }
+        public static int Rakam(string Password) // Şifrede ki rakam sayısını hesaplayan metod.
+        {
+            int sayac = 0;
+            for (int i = 0; i < Password.Length; i++) 
+                if (char.IsDigit(Password[i]))
+                    sayac += 1;
+            return sayac*10;
+        }
+        public static int Sembol(string Password) // Şifrede ki sembol sayısını hesaplayan metod.
+        {
+            int sayac = 0;
+            for (int i = 0; i < Password.Length; i++)
+                if (!char.IsLetterOrDigit(Password[i])) 
+                    sayac += 1;
+            return sayac * 10;
+        }
+
+        public static int ParolaPuanı(string Password) // Şifrede ki karakterlerin puanını hesaplayan metod.
+        {
+            if (Password == null)
+            {
+                return 0;
+            }
+
+            int küçükHarfPuanı = KüçükHarf(Password);
+            int büyükHarfPuanı = BüyükHarf(Password);
+            int rakamPuanı = Rakam(Password);
+            int semboolPuanı = Sembol(Password);
+
+            return küçükHarfPuanı + büyükHarfPuanı + rakamPuanı + semboolPuanı;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cevap = null;
+            do
+            {
+                Console.ForegroundColor= ConsoleColor.White;
+                int puan = 0;
+                Console.WriteLine("Bir şifre oluşturunuz"); // Burda şifre oluşturuluyor.
+
+                string Sifre = Console.ReadLine();
+
+                
+
+                int Kücük= ParolaGuvenligi.KüçükHarf(Sifre), kucuk=ParolaGuvenligi.KüçükHarf(Sifre);
+                int Büyük = ParolaGuvenligi.BüyükHarf(Sifre), buyuk=ParolaGuvenligi.BüyükHarf(Sifre);               
+                int Rakam = ParolaGuvenligi.Rakam(Sifre), rakam=ParolaGuvenligi.Rakam(Sifre);
+                int Sembol = ParolaGuvenligi.Sembol(Sifre), sembol=ParolaGuvenligi.Sembol(Sifre);
+                
+
+               
+                if(Kücük>20)   Kücük=20; // Şifrede küçük harflerden gelen puan 20 den büyüse 20 ye eşitleniyor.
+                if(Büyük>20)   Büyük=20; // Şifrede büyük harflerden gelen puan 20 den büyüse 20 ye eşitleniyor.
+                if (Rakam>20)   Rakam=20; // Şifrede rakamlardan gelen puan 20 den büyüse 20 ye eşitleniyor.
+
+                if (kucuk <= 0) // Şifrede küçük harf olup olmadığına bakılıyor.
+                { 
+                    Console.WriteLine("En az 1 küçük harf giriniz");
+                }
+                else
+                {     
+                    if(buyuk <= 0) // Şifrede büyük harf olup olmadığına bakılıyor.
+                    {
+                        Console.WriteLine("En az 1 büyük harf giriniz");
+                    }
+                    else
+                    { 
+                        if(rakam<=0) // Şifrede rakam olup olmadığına bakılıyor.
+                        {
+                            Console.WriteLine("En az 1 rakam giriniz");
+                        }
+                        else
+                        {
+                            if(sembol<=0) // Şifrede sembol olup olmadığına bakılıyor.
+                            {
+                                Console.WriteLine("En az 1 sembol giriniz");
+                            }
+                            else
+                            {
+                                for (int i = 0; i < Sifre.Length; i++) // Şifrede boşluk varsa şifreyi kabul etmiyor.
+                                {
+                                    if (char.IsWhiteSpace(Sifre[i]))
+                                    {
+                                        Console.WriteLine("Şifreniz karakter olarak boşluk içeremez");
+                                        break;
+                                    }
+                                } 
+                                if (Sifre.Length < 9) // Şifrenin minumum 9 karakterden oluşup oluşmadığına bakıyor.
+                                {
+                                    Console.WriteLine("Minumum 9 karakterli olmalıdır.");
+                                }
+                                else if (Sifre.Length == 9) // Eğer şifre 9 karakterse şifre puanına 10 puan ekliyor.
+                                {
+                                    puan += 10;
+                                }
+                                puan = Kücük + Büyük + Rakam + Sembol;
+                                if (puan < 70)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.WriteLine("Şifreniz kolay bir şifredir.");
+                                    Console.ForegroundColor= ConsoleColor.Red;
+                                    Console.WriteLine("        Şifrenizde ki küçük harf sayısı :" + kucuk / 10); // Şifrede ki küçük harf sayısını gösteriyor.
+                                    Console.WriteLine("        Şifrenizde ki büyük harf sayısı :" + buyuk / 10); // Şifrede ki büyük harf sayısını gösteriyor.
+                                    Console.WriteLine("        Şifrenizde ki rakam  sayısı :" + rakam / 10); // Şifrede ki rakam sayısını gösteriyor.
+                                    Console.WriteLine("        Şifrenizde ki sembol sayısı :" + sembol / 10); // Şifrede ki sembol sayısını gösteriyor.
+                                    Console.WriteLine("   Şifrenizi değiştirmek istermisiniz (e/h)"); cevap = Console.ReadLine();
+                                }
+                                else if (puan < 90 && 70 <= puan)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                                    Console.WriteLine("Şifreniz normal zorlukta bir şifredir.");
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("       Şifrenizde ki küçük harf sayısı :" + kucuk / 10); // Şifrede ki küçük harf sayısını gösteriyor.
+                                    Console.WriteLine("       Şifrenizde ki büyük harf sayısı :" + buyuk / 10); // Şifrede ki büyük harf sayısını gösteriyor.
+                                    Console.WriteLine("       Şifrenizde ki rakam  sayısı :" + rakam / 10); // Şifrede ki rakam sayısını gösteriyor.
+                                    Console.WriteLine("       Şifrenizde ki sembol sayısı :" + sembol / 10); // Şifrede ki sembol sayısını gösteriyor.
+                                    Console.WriteLine("   Şifrenizi değiştirmek istermisiniz (e/h)"); cevap = Console.ReadLine();
+                                }
+                                else if (puan <= 100 && 90 <= puan)
+                                {
+                                    Console.WriteLine("Güçlü bir şifre oluşturdunuz.");
+                                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                    Console.WriteLine("      Şifrenizde ki küçük harf sayısı :" + kucuk / 10); // Şifrede ki küçük harf sayısını gösteriyor.
+                                    Console.WriteLine("      Şifrenizde ki büyük harf sayısı :" + buyuk / 10); // Şifrede ki büyük harf sayısını gösteriyor.
+                                    Console.WriteLine("      Şifrenizde ki rakam  sayısı :" + rakam / 10); // Şifrede ki rakam sayısını gösteriyor.
+                                    Console.WriteLine("      Şifrenizde ki sembol sayısı :" + sembol / 10); // Şifrede ki sembol sayısını gösteriyor.
+                                    Console.WriteLine("   Başka bir şifre yazmak istiyormusunuz (e/h)"); cevap = Console.ReadLine();
+                                } 
+                                
+                            }
+                        }
+                    }
+                }
+            } while (cevap == "e");
+            Console.ReadLine();
+        }
+    }
+}
